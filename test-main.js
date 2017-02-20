e607b8d3459ab71310a1d387a64141ab78bb59e0
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-02-20 20:21:24 +0100

    COpy content of main requirejs file to test-main config to keep easy test module definitions

--- test-main.js
+++ test-main.js
@@ -15,7 +15,41 @@ Object.keys(window.__karma__.files).forEach(function (file) {
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
   baseUrl: '/base',
+  paths: {
+      /* Modules and files */
+      'MainModule': 'application/main/MainModule',
+      'MainConfig': 'application/main/MainConfig',
+      'AthleteModule': 'application/athlete/AthleteModule',
+      'AthleteConfig': 'application/athlete/AthleteConfig',
+      'AthleteStates': 'application/athlete/shared/AthleteStates',
+      'AthleteMainController': 'application/athlete/main/AthleteMainController',
 
+      'sidebarDirective': 'application/shared/directives/sidebar/sidebarDirective',
+
+      /* Libs */
+      'angular': 'libs/angular.min',
+      'angular-ui-router': 'libs/angular-ui-router.min',
+      'angular-aria': 'libs/angular-aria.min',
+      'angular-animate': 'libs/angular-animate.min',
+      'angular-material': 'libs/angular-material.min'
+  },
+  shim: {
+      'angular': {
+          exports: 'angular'
+      },
+      'angular-ui-router': {
+          deps: ['angular']
+      },
+      'angular-material': {
+          deps: ['angular', 'angular-aria', 'angular-animate']
+      },
+      'angular-aria': {
+          deps: ['angular']
+      },
+      'angular-animate': {
+          deps: ['angular']
+      }
+  },
   // dynamically load all test files
   deps: allTestFiles,
 
