7f2c73bd700562caf2c8c31c3e567a2122d6c55f
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-02-24 16:39:17 +0100

    Refactor

--- public/scripts/application/athlete/training/AthleteTrainingController.js
+++ public/scripts/application/athlete/training/AthleteTrainingController.js
@@ -1,13 +1,12 @@
-define(['AthleteModule', 'exerciseCardDirective', 'exercisePlanDirective', 'Exercises'], function(AthleteModule) {
-    AthleteModule.controller('AthleteTrainingController', function(Exercises) {
+define(['AthleteModule', 'ExerciseFactory', 'exerciseCardDirective', 'exercisePlanDirective', 'Exercises'], function(AthleteModule) {
+    AthleteModule.controller('AthleteTrainingController', function(Exercises, ExerciseFactory) {
         var vm = this;
 
         vm.exercises = Exercises;
-        vm.trainingSessions = {};
         vm.training = [];
 
-        vm.addToRoutine = function(exercise) {
-            vm.training.push(exercise);
+        vm.addToRoutine = function(exerciseName) {
+            vm.training.push(ExerciseFactory.create(exerciseName));
         }
 
         vm.removeFromRoutine = function(index) {
--- public/scripts/application/shared/directives/exercise/exercisePlanDirective.js
+++ public/scripts/application/shared/directives/exercise/exercisePlanDirective.js
@@ -1,27 +1,22 @@
 define(['MainModule', 'angular'], function(MainModule, angular) {
-    MainModule.directive('exercisePlanDirective', function($filter) {
+    MainModule.directive('exercisePlanDirective', function() {
         return {
             restrict: 'E',
             scope: {
-                'exerciseName': '=',
+                'exercise': '=',
                 'onCancel': '&'
             },
             templateUrl: 'scripts/application/shared/directives/exercise/exercisePlanDirectiveTemplate.html',
             link: function(scope) {
-                scope.exercise = {
-                    name: scope.exerciseName,
-                    sets: []
-                }
-
-                scope.exercise.sets.push({})
+                scope.exercise.sets.push({});
 
                 scope.addSet = function() {
-                    scope.exercise.sets.push({});
-                }
+                    scope.exercise.addSet({});
+                };
 
                 scope.remove = function(index) {
-                    scope.exercise.sets.splice(index, 1); 
-                }
+                    scope.exercise.removeSet(index);
+                };
             }
         };
     })
