124a136fbc764aaccb11eff9f11fcd9ed905d4f7
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-02-24 17:09:50 +0100

    Add ui.bootstrap as dependency to main module

--- public/scripts/application/main/MainModule.js
+++ public/scripts/application/main/MainModule.js
@@ -1,5 +1,5 @@
-define(['angular', 'angular-ui-router', 'AthleteModule', 'angular-material'], function(angular) {
-    var m = angular.module('MainModule', ['ui.router', 'AthleteModule', 'ngMaterial']);
+define(['angular', 'angular-ui-router', 'AthleteModule', 'angular-material', 'ui-bootstrap-tpls'], function(angular) {
+    var m = angular.module('MainModule', ['ui.router', 'AthleteModule', 'ngMaterial', 'ui.bootstrap']);
 
     m.init = function() {
         require(['MainConfig'], function() {
