bf35e48df8cff6f2d7f08cccc9df644fe93f97fb
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-03-31 10:24:12 +0200

    refactor slightly training directive, add basic functionality to abc

--- public/scripts/application/athlete/board/AthleteBoardController.js
+++ public/scripts/application/athlete/board/AthleteBoardController.js
@@ -1,11 +1,32 @@
-define(['AthleteModule'], function(AthleteModule) {
-    AthleteModule.controller('AthleteBoardController', function() {
+define(['AthleteModule', 'AthleteTrainingService', 'Exercises'], function(AthleteModule) {
+    AthleteModule.controller('AthleteBoardController', function(AthleteTrainingService, Exercises) {
         var vm = this;
-        
-        vm.train = train;
 
-        function train() {
-            
+        vm.train = train;
+        vm.init = init;
+        vm.backToList = backToList;
+        vm.startTraning = startTraning;
+
+        vm.init();
+
+        function init() {
+            vm.currentTraining = {};    
+            vm.trainings = AthleteTrainingService.get();
+            vm.trainingChosen = false;
+            vm.exercises = Exercises;
+        }
+
+        function train(training) {
+            vm.currentTraining = training;
+            vm.trainingChosen = true;
+        }
+
+        function backToList() {
+            vm.trainingChosen = false;
+        }
+
+        function startTraning(training) {
+            vm.trainingStarted = true;
         }
     })
 })
