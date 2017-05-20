9faffb4b6d11ddc2e12e67b5ef067e40ae5b739e
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-05-20 12:25:48 +0200

    Rename test description

--- public/scripts/tests/e2e/tests/athlete/athlete-spec.js
+++ public/scripts/tests/e2e/tests/athlete/athlete-spec.js
@@ -27,7 +27,7 @@ describe('Athlete spec', function () {
             athletePage.sidebar.clickButton('training');
             trainingState = athletePage.Training;
         })
-/*
+
         it('When clicking \'Training\' state URL should change', function () {
             expect(athletePage.getUrl()).toEqual('http://localhost:3000/#!/athlete/training');
         });
@@ -58,8 +58,8 @@ describe('Athlete spec', function () {
 
             expect(newTraining.getExercisePlan('Rows').getExerciseName()).toEqual('Rows');
         }); 
-*/
-        it('When saving an edited training the number of trainings should be as previously', function () {
+
+        it('When saving an edited training the number of trainings should not increment', function () {
 
             trainingState.getTrainings().count().then(function(count) {
                 var presentTrainingCount = count;
